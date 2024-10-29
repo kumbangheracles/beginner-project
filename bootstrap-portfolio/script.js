fc176a36ca50eee70f529ff72b036d15d030517c
kumbangheracles <kumbangheracles@gmail.com>
2024-10-29 12:47:28 +0700

    initial commit

---
+++ bootstrap-portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".lead", {
+    strings: ["I am an informatics engineering student with a strong foundational understanding of programming","2 years of experience as a student", "and I am currently learning about web development"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
